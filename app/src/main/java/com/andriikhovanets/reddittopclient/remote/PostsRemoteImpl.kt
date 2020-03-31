3e0edc703922326bac78c5a1250958d5c5ad280b
andriihovanec <andriihovanec@gmail.com>
2020-03-31 09:00:11 +0300

    added remote layer

---
+++ app/src/main/java/com/andriikhovanets/reddittopclient/remote/PostsRemoteImpl.kt
@@ -0,0 +1,4 @@
+package com.andriikhovanets.reddittopclient.remote
+
+class PostsRemoteImpl {
+}
---
+++ app/src/main/java/com/andriikhovanets/reddittopclient/remote/core/BaseResponse.kt
@@ -0,0 +1,6 @@
+package com.andriikhovanets.reddittopclient.remote.core
+
+open class BaseResponse(
+    val success: Int,
+    val message: String
+)
